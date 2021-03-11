19949391abc09a9b2e3f2ccf5240cea74c2f1b32
karl bertin <bertin_k@qwasar.io>
2021-03-11 15:55:41 +0000

    js-quest05 updated: ex03/ created, and (ex03/)my_add.js created.

---
+++ ex03/my_add.js
@@ -0,0 +1,16 @@
+/*
+**
+** QWASAR.IO -- my_add
+**
+**
+** @param {Integer} param_1
+** @param {Integer} param_2
+** @return {integer}
+
+**
+*/
+
+
+function my_add(param_1, param_2) {
+  return ( param_1 + param_2 );
+};
