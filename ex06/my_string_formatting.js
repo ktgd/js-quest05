74edb783b00aa44806da811f51d38345577bdcc3
karl bertin <bertin_k@qwasar.io>
2021-03-11 16:08:46 +0000

    js-quest05 updated: ex06/ created, and (ex06/)my_string_formatting.js created.

---
+++ ex06/my_string_formatting.js
@@ -0,0 +1,15 @@
+/*
+**
+** QWASAR.IO -- my_string_formatting
+**
+**
+** @param {String} param_1
+** @param {String} param_2
+** @param {Integer} param_3
+**
+*/
+
+
+function my_string_formatting(param_1, param_2, param_3) {
+  console.log("Hello, my name is " + param_1 + " " + param_2 + ", I'm " + param_3 + ".");
+};
